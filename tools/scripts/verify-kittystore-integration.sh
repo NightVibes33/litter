92453285e97b7b286514b01b578bcf1a70c7ada1
Bobby Tatum <NightVibes33@proton.me>
2026-08-27 17:57:48 +0000

    Resolve Nyxian ZSign JSON headers

--- tools/scripts/verify-kittystore-integration.sh
+++ tools/scripts/verify-kittystore-integration.sh
@@ -223,7 +223,7 @@ require_grep "emexDE target excludes private framework stubs from bundle" "LindC
 require_grep "emexDE runtime cleanup removes private framework stubs" "Removed nested emexDE private framework stub" "apps/ios/project.yml"
 require_grep "emexDE runtime cleanup covers FrontBoardServices" "FrontBoardServices.framework" "apps/ios/project.yml"
 require_grep "emexDE target uses upstream bridge" "SWIFT_OBJC_BRIDGING_HEADER: ../../ThirdParty/EmexDE/Source/Nyxian/NXBridge.h" "apps/ios/project.yml"
-require_grep "emexDE target disables collision-prone header maps" "USE_HEADERMAP: \"NO\"" "apps/ios/project.yml"
+require_grep "emexDE target resolves upstream root headers" "ThirdParty/EmexDE/Source \$(SRCROOT)/../../ThirdParty/EmexDE/Source/Frameworks \$(SRCROOT)/../../ThirdParty/EmexDE/Source/Nyxian/LindChain/ProcEnvironment/LiveContainer/ZSign/common \$(SRCROOT)/../../ThirdParty/EmexDE/Source/Nyxian" "apps/ios/project.yml"
 require_grep "emexDE target resolves upstream root headers" "ThirdParty/EmexDE/Source \$(SRCROOT)/../../ThirdParty/EmexDE/Source/Frameworks \$(SRCROOT)/../../ThirdParty/EmexDE/Source/Nyxian" "apps/ios/project.yml"
 require_grep "emexDE target embeds LiveProcess" "target: LiveProcess" "apps/ios/project.yml"
 require_grep "emexDE route boots upstream projects" "ContentViewController()" "apps/ios/Sources/EmexDEEmbedded/EmexDEEmbeddedFactory.swift"
